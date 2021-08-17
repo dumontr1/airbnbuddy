921bbed71422ca3073de57e594774408c08b742c
Robin Dumont <robin.dumont@kedgebs.com>
2021-08-17 10:49:10 +0200

    routes setup done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :offers, only: [:index, :show, :create, :destroy] do
+    resources :bookings, only: [:create, :update]
+  end
+  resources :bookings, only: :index
+  namespace :owner do
+    resources :bookings, only: :index
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
