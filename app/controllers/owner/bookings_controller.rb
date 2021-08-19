c708451e0948d28e4c6d4539659935e4ee5bcca1
Matteo Mariot <matt.mariot72@gmail.com>
2021-08-19 17:17:02 +0200

    changed my_offers to my_bookings in owner folder (V+C)

--- app/controllers/owner/bookings_controller.rb
+++ app/controllers/owner/bookings_controller.rb
@@ -1,6 +1,6 @@
 class Owner::BookingsController < ApplicationController
   def index
-    @my_offers = Booking.where(user_id: current_user)
+    @my_bookings = Booking.where(user_id: current_user)
     @offers = Offer.all
   end
 end
